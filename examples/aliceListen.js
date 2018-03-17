04ef4ad3939acca1d60c230145be2d77a9d33efe
fletcherist <romanovexports@gmail.com>
2018-03-17 23:31:04 +0300

    [feature]: Make some comments

--- examples/aliceListen.js
+++ examples/aliceListen.js
@@ -2,7 +2,10 @@ const Alice = require('../src/index')
 const alice = new Alice()
 
 alice.command('архангельск', async (ctx) => {
+  console.log(ctx)
   return ctx.reply('asasdad')
 })
+
+
 
 alice.listen('/', 8080)
--- src/alice.js
+++ src/alice.js
@@ -7,8 +7,10 @@ const selectCommand = req => req.request.command
 class Alice {
   constructor() {
     this.commands = []
+    this.any = 
   }
 
+  /* @TODO: Implement watchers (errors, messages) */
   on() {
 
   }
@@ -20,32 +22,49 @@ class Alice {
     })
   }
 
+  any(callback) {
+
+  }
+
   async handleRequestBody(req) {
     const requestedCommandName = selectCommand(req)
     const requestedCommands = this.commands.filter(command =>
       command.name === requestedCommandName)
 
+    /*
+     * Command has been found in the list.
+     * Lets run its handler
+     *
+     * Команда нашлась в списке.
+     * Запускаем её обработчик.
+     */
     if (requestedCommands.length !== 0) {
       const requestedCommand = requestedCommands[0]
       return await requestedCommand.callback.call(this, new Ctx({
         req: req
       }))
     }
+
+    /*
+     * Такой команды не было зарегестрировано.
+     * Переходим в обработчик исключений.
+     */
     console.log(requestedCommands)
   }
 
   /*
-   * Handles every request
-   * and find a command and action for that
+   * Метод создаёт сервер, который слушает указанный порт.
+   * Когда на указанный URL приходит POST запрос, управление
+   * передаётся в @handleRequestBody
+   *
+   * При получении ответа от @handleRequestBody, результат
+   * отправляется обратно.
    */
   listen(callbackUrl = '/', port = 80) {
-
     const app = express()
     app.use(bodyParser.json())
     app.post(callbackUrl, async (req, res) => {
-      console.log(req.body)
       const replyMessage = await this.handleRequestBody(req.body)
-      console.log(replyMessage)
       res.send(replyMessage)
     })
     app.listen(port, () => console.log(`Alice is listening on ${port} port`))
