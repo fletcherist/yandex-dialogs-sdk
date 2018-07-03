cee8ec7341eb0ae5a6a299587d2b8e6f66357c3d
fletcherist <fletcherist@yandex-team.ru>
2018-07-03 21:30:09 +0300

    feat: add webhook request/response types

---
+++ src/types/webhook.d.ts
@@ -0,0 +1,44 @@
+export interface WebhookRequest {
+  meta: {
+      locale: string;
+      timezone: string;
+      client_id: string;
+  }
+  request: {
+      command: string;
+      original_utterance: string;
+      type: 'SimpleUtterance' | 'ButtonPressed',
+      markup?: {
+          dangerous_context?: true;
+      },
+      payload: object;
+  }
+  session: {
+      new: boolean;
+      message_id: number;
+      session_id: string;
+      skill_id: string;
+      user_id: string;
+  }
+  version: string;
+}
+
+export interface WebhookResponse {
+  response: {
+      text: string;
+      tts?: string;
+      buttons?: {
+          title?: string;
+          payload?: object;
+          url?: string;
+          hide?: boolean;
+      }[]
+      end_session: boolean;
+  }
+  session: {
+      message_id: number;
+      session_id: string;
+      user_id: string;
+  }
+  version: string;
+}
