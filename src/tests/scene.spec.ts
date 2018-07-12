72166c898995dce037772f29c74b1b0815e67786
Vanya Klimenko <v@vanyaklimenko.ru>
2018-07-12 15:43:28 +0300

    cover arrays in registerScene

--- src/tests/scene.spec.ts
+++ src/tests/scene.spec.ts
@@ -1,6 +1,19 @@
+import Alice from "..";
+
 const Scene = require('../scene')
 
 test('creating scene with name', () => {
   const scene = new Scene('testName')
   expect(scene.name).toBe('testName')
 })
+
+test('registering an array of scenes', () => {
+  const alice = new Alice()
+  const scene1 = new Scene('scene1')
+  const scene2 = new Scene('scene2')
+
+  alice.registerScene([scene1, scene2])
+
+  expect(alice.scenes.length).toBe(2)
+})
+
