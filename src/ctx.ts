6c0ec8896bf67b5b1ca59f832ac471d738870f6f
fletcherist <fletcherist@yandex-team.ru>
2018-07-14 11:27:17 +0300

    feature: add tests for image

--- src/ctx.ts
+++ src/ctx.ts
@@ -9,6 +9,7 @@ import { CtxInterface } from './types/ctx'
 import { CommandInterface } from './types/command'
 import { BigImageCard } from './types/card'
 import { image, bigImageCard, itemsListCard } from './card'
+import reply from './reply'
 
 export default class Ctx implements CtxInterface {
   public req: WebhookRequest
@@ -79,8 +80,8 @@ export default class Ctx implements CtxInterface {
 
   public async replyWithImage(params: string | BigImageCard) {
     if (typeof params === 'string') {
-
-      const message = this._createReply(bigImageCard(image(params)))
+      const message = this._createReply(reply(bigImageCard(image(params))))
+      console.log(message)
       return this._sendReply(message)
     } else {
       const message = this._createReply(bigImageCard(params))
