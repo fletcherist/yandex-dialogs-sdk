9c89f1eb1064c3294e418494dba40fd84b602420
fletcherist <fletcherist@yandex.ru>
2018-07-28 10:09:43 +0300

    fix logger

--- src/middlewares/loggerMiddleware.ts
+++ src/middlewares/loggerMiddleware.ts
@@ -58,32 +58,26 @@ export default function createLoggerMiddleware(opts: ILogger = {}) {
         if (isInitialized) {
             return ctx
         }
-        let isInitialized = false
-        return ctx => {
-            if (isInitialized) {
-                return ctx
+
+        log.setLevel(opts.level || 0)
+
+        ctx.eventEmitter.subscribe(EVENT_MESSAGE_RECIEVED, logEvent)
+        ctx.eventEmitter.subscribe(EVENT_MESSAGE_SENT, logEvent)
+        ctx.eventEmitter.subscribe(EVENT_MESSAGE_NOT_SENT, logEvent)
+        ctx.eventEmitter.subscribe(EVENT_MESSAGE_PROXIED, logEvent)
+        ctx.eventEmitter.subscribe(EVENT_MESSAGE_PROXY_ERROR, logEvent)
+
+        function logEvent(event) {
+            try {
+                log[eventTypes[event.type]](
+                    [`{${chalk.cyan(event.type)}}`, event.data].filter(Boolean).join(' ')
+                )
+            } catch (error) {
+                log.error(`Cant log "${event.type}"`)
             }
-
-            log.setLevel(opts.level || 0)
-
-            ctx.eventEmitter.subscribe(EVENT_MESSAGE_RECIEVED, logEvent)
-            ctx.eventEmitter.subscribe(EVENT_MESSAGE_SENT, logEvent)
-            ctx.eventEmitter.subscribe(EVENT_MESSAGE_NOT_SENT, logEvent)
-            ctx.eventEmitter.subscribe(EVENT_MESSAGE_PROXIED, logEvent)
-            ctx.eventEmitter.subscribe(EVENT_MESSAGE_PROXY_ERROR, logEvent)
-
-            function logEvent(event) {
-                try {
-                    log[eventTypes[event.type]](
-                        [`{${chalk.cyan(event.type)}}`, event.data].filter(Boolean).join(' ')
-                    )
-                } catch (error) {
-                    log.error(`Cant log "${event.type}"`)
-                }
-            }
-
-            isInitialized = true
-            return ctx
         }
+
+        isInitialized = true
+        return ctx
     }
 }
