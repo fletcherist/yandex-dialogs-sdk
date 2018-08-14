eda447120bf065ee1d2b3fd9ecec13aaad3d8062
fletcherist <fletcherist@yandex-team.ru>
2018-08-14 17:20:33 +0300

    feat: add tests for button

--- tests/reply.spec.ts
+++ tests/reply.spec.ts
@@ -1,4 +1,4 @@
-import { Reply } from '../dist';
+import { Reply, Markup } from '../dist';
 import { getRandomText } from './testUtils';
 
 describe('Alice Reply (static method) suite', () => {
@@ -26,5 +26,31 @@ describe('Alice Reply (static method) suite', () => {
       tts: expected.tts,
       end_session: true,
     });
+  });
+});
+
+describe('Markup suite', () => {
+  test('Markup button with text constructor', () => {
+    const expected = {
+      title: 'foo',
+      payload: {
+        title: 'foo',
+      },
+    };
+    const button = Markup.button(expected.title);
+    expect(button).toEqual(expected);
+  });
+
+  test('Markup button with object constructor', () => {
+    const expected = {
+      title: 'foo',
+      hide: true,
+      url: '',
+      payload: {
+        title: 'foo',
+      },
+    };
+    const button = Markup.button(expected);
+    expect(button).toEqual(expected);
   });
 });
